fcb5f277e4a00862208af701d905ac789053b650
Matt Copperwaite <mattcopp+git@gmail.com>
2013-12-09 21:17:35 +0000

    Adding basic site framework

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "port": 3000,
+    "title": "South London Makerspace",
+    "static_dir": "./static",
+    "logo": "./static/img/logo.png",
+    "templates_dir": "./templates",
+    "secret_key": "!!!! CHANGE ME !!!",
+    "apps": {
+//      "account": require("./apps/account/app.js"),
+        "membership": require("./apps/membership-system/app.js"),
+    }
+}
---
+++ site.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var _ = require("underscore");
+
+var config = require("./config.js");
+
+var site = express();
+site.use(express.logger());
+site.use(config.static_dir, express.static("."));
+
+// load each app in to the site HAS to be a better way
+_.each(config.apps, function (app, location) {
+    var app_routes = app.app().routes;
+    _.each(app_routes.get, function (route) {
+        site.get("/" + location + route.path, route.callbacks)
+    });
+});
+
+site.listen(config.port);
+console.log("Started listening on port " + config.port);
