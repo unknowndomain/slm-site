5c5b77a8614452e0afee1ae2b5fc8405a73a1ca3
Matt Copperwaite <mattcopp+git@gmail.com>
2013-12-13 16:32:25 +0000

    Passing through useful objects to app

--- site.js
+++ site.js
@@ -4,14 +4,21 @@ var _ = require("underscore");
 var config = require("./config.json");
 
 var db = require("./database")(config.db.connection_string);
+var jinja = require("jinja");
 
 var site = express();
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', '.jinja.html');
+app.set('views', './templates');
+
 site.use(express.logger());
 site.use("/static", express.static(config.static_dir));
 
 _.each(config.apps, function (app) {
     var module = require(app.module);
-    site.use(app.route, module.app(db));
+    site.use(app.route, module.app(db, site, config));
 });
 
 site.listen(config.port);
