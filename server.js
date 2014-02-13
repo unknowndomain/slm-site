1a43868457a55bd5555f07adc4fec2559814ad27
Matt Copperwaite <mattcopp+git@gmail.com>
2014-02-13 21:59:25 +0000

    added messages local var for messages to be pushed to

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ site.use(express.bodyParser());
 site.use(function (req, res, next) {
     res.locals.email = req.session.email;
     res.locals.path = req.path
+    res.locals.messages = []
     next();
 });
 
